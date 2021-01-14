75a140fe21de0e29bfa32e1a472d03b6c19a6948
Roman Golchin <romagolchin@gmail.com>
2021-01-14 11:38:52 +0300

    add simple tree serializer for automaton grammar

--- metamodeling/src/main/kotlin/org/golchin/automata/AutomatonParseTree.kt
+++ metamodeling/src/main/kotlin/org/golchin/automata/AutomatonParseTree.kt
@@ -1,9 +1,29 @@
 package org.golchin.automata
 
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.misc.TestRig
+import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+import org.golchin.grammar.IndentedWriter
+import java.io.FileWriter
 
 fun main() {
-    val inputStream = ClassLoader.getSystemResourceAsStream("automaton.src")
-    System.setIn(inputStream)
+    val name = "automaton.src"
+    val inputStream = ClassLoader.getSystemResourceAsStream(name)
+    val stream = CharStreams.fromStream(inputStream)
+    val lexer = AutomatonLexer(stream)
+    // create a buffer of tokens pulled from the lexer
+    val tokens = CommonTokenStream(lexer)
+    // create a parser that feeds off the tokens buffer
+    val parser = AutomatonParser(tokens)
+    val tree: ParseTree = parser.automaton()
+    val writer = FileWriter("output.txt")
+    writer.use {
+        val listener = WritingParseTreeListener(parser.ruleNames, parser.vocabulary, IndentedWriter(writer))
+        ParseTreeWalker.DEFAULT.walk(listener, tree)
+    }
+
+    System.setIn(ClassLoader.getSystemResourceAsStream(name))
     TestRig.main(arrayOf("org.golchin.automata.Automaton", "automaton", "-gui"))
 }
--- metamodeling/src/main/kotlin/org/golchin/automata/ParseTree.kt
+++ metamodeling/src/main/kotlin/org/golchin/automata/ParseTree.kt
@@ -4,6 +4,8 @@ import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.misc.TestRig
 import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.tree.ParseTreeListener
+import org.antlr.v4.runtime.tree.ParseTreeWalker
 import java.io.FileInputStream
 
 fun parse(filePath: String) {
@@ -16,6 +18,7 @@ fun parse(filePath: String) {
     val parser = GrammarParser(tokens)
     val tree: ParseTree = parser.source()
     println(tree.toStringTree(parser))
+    ParseTreeWalker.DEFAULT.walk(null, tree)
     System.setIn(FileInputStream(filePath))
     TestRig.main(arrayOf("org.golchin.automata.Grammar", "source", "-gui"))
 }
